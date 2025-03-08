4804128ad42d9228407824e85038c80426dfb4a2
hrederik <32139140+hrederik@users.noreply.github.com>
2025-03-08 01:27:35 -0800

    documented offset popup

--- lib/presentation/widgets/offset_popup.dart
+++ lib/presentation/widgets/offset_popup.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
 
+/// A widget that displays a popup menu with an offset.
+///
+/// This widget provides the user with a selection of actions,
+/// presented as menu items.
 class OffsetPopup extends StatelessWidget {
+  /// Creates an instance of [OffsetPopup].
+  ///
+  /// Parameters:
+  ///   * [clicked]:  A function to be called when the popup menu is opened.
+  ///   * [selected]: A function to be called when a menu item is selected.  Takes the value of the selected item.
+  ///   * [canceled]: A function to be called when the popup menu is canceled (e.g., by tapping outside the menu).
   const OffsetPopup({
     super.key,
     required VoidCallback clicked,
     required ValueChanged<int> selected,
     required VoidCallback canceled,
-  }) : _clicked = clicked, _selected = selected, _canceled = canceled;
+  })  : _clicked = clicked,
+        _selected = selected,
+        _canceled = canceled;
 
+  /// Function to be called when the popup menu is opened.
   final VoidCallback _clicked;
+
+  /// Function to be called when a menu item is selected.
   final ValueChanged<int> _selected;
+
+  /// Function to be called when the popup menu is canceled.
   final VoidCallback _canceled;
 
   @override
@@ -17,17 +34,23 @@ class OffsetPopup extends StatelessWidget {
     return SizedBox(
       height: 80.0,
       width: 80.0,
-      child: PopupMenuButton<int>(
+      child: PopupMenuButton(
+        // Called when the menu is opened.
         onOpened: _clicked,
+        // Called when a menu item is selected.
         onSelected: _selected,
+        // Called when the menu is canceled.
         onCanceled: _canceled,
+        // Creates the list of menu items.
         itemBuilder: (context) => [
           const PopupMenuItem(
             value: 0,
             child: Text(
               "Enter fullscreen",
               style: TextStyle(
-                  color: Colors.black, fontWeight: FontWeight.w700),
+                color: Colors.black,
+                fontWeight: FontWeight.w700,
+              ),
             ),
           ),
           const PopupMenuItem(
@@ -35,21 +58,24 @@ class OffsetPopup extends StatelessWidget {
             child: Text(
               "Exit fullscreen",
               style: TextStyle(
-                  color: Colors.black, fontWeight: FontWeight.w700),
+                color: Colors.black,
+                fontWeight: FontWeight.w700,
+              ),
             ),
           ),
         ],
+        // Widget displayed to open the menu.
         icon: Container(
           height: double.infinity,
           width: double.infinity,
           decoration: const ShapeDecoration(
-              color: Colors.blue,
-              shape: StadiumBorder(
-                side: BorderSide(color: Colors.white, width: 2),
-              )
+            color: Colors.blue,
+            shape: StadiumBorder(
+              side: BorderSide(color: Colors.white, width: 2),
+            ),
           ),
           child: const Icon(Icons.menu, color: Colors.white),
-        )
+        ),
       ),
     );
   }
